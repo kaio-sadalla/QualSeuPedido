7bc9bb299ac94db9910c89d04b92a5572c5fc681
Kaio Sadalla <ksadalla@gmail.com>
2026-02-09 00:11:07 -0300

    This commit add pages layout with login and main page

---
+++ src/main/java/br/com/qualseupedido/dao/GenericoDAO.java
@@ -0,0 +1,16 @@
+package br.com.qualseupedido.dao;
+
+public interface GenericoDAO {
+    default void criar(){
+
+    }
+    default void deletar(){
+
+    }
+    default void obter(){
+
+    }
+    default void atualizar(){
+
+    }
+}
---
+++ src/main/java/br/com/qualseupedido/dao/MensagemDAO.java
@@ -0,0 +1,9 @@
+package br.com.qualseupedido.dao;
+
+import javax.inject.Named;
+import javax.persistence.NamedQuery;
+
+//@NamedQuery()
+public class MensagemDAO {
+
+}
